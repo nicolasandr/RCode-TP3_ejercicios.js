2c7b41e58ca4e1a48c167124daa3f95d1cff7528
nicolas andrade <nicolasandrade328@gmail.com>
2022-05-31 01:32:21 -0300

    agregamos ejercicio 5 terminado

--- js/5_2_func_ejercicio.js
+++ js/5_2_func_ejercicio.js
@@ -4,27 +4,19 @@ le pasa como argumento. A partir de la cadena que se le pasa, la función
 determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o
 por una mezcla de ambas.
 */
-//incompleto
-function esMayuscula(letra)
-{
-    return letra === letra.toUpperCase();
-}
 
-function esMinuscula(letra)
-{
-    return letra === letra.toLowerCase();
-}   
-let cadena=prompt('ingrese cadena de texto:'); 
-for(var i = 0; i < cadena.length; i++)
-{
-    var letraActual = cadena.charAt(i);
-    if(esMayuscula(letraActual))
-    {
-        alert("La letra " + letraActual + " es mayúscula");
+function cadenaDeTexto(cadena){
+
+   if(cadena === cadena.toUpperCase()){
+      return "La cadena esta formada por solo mayusculas";
+
     }
-
-    if(esMinuscula(letraActual))
-    {
-        alert("La letra " + letraActual + " es minúscula");
-    }       
+   else if(cadena === cadena.toLowerCase()){
+       return "La cadena esta formada por solo minusculas";
+   }
+   else{
+       return "La cadena esta formada por mayusculas y minusculas";
+   }
 }
+let cadena=prompt('ingrese cadena de texto:'); 
+document.write(cadenaDeTexto(cadena));
